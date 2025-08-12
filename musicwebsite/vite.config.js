cea776d3243808b4d8e8cf8ec0d9f450fd21b293
Alexia Ventura <alexiaventura212@gmail.com>
2025-08-11 20:51:46 -0700

    Trying to fix problem

--- musicwebsite/vite.config.js
+++ musicwebsite/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/',
+  base: "/alexiaventuramusic.github.io",
   plugins: [react()],
 })
 
