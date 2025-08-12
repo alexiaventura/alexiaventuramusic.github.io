99b7039cf1647854ac9d56e547768da33542e21c
Alexia Ventura <alexiaventura212@gmail.com>
2025-08-11 18:24:16 -0700

    ?

---
+++ musicwebsite/src/routes/Contact.jsx
@@ -0,0 +1,12 @@
+import { Outlet, Link } from "react-router-dom"
+
+function Contact() {
+    return (
+        <div>
+            <h2>Contact Page</h2>
+            <p>This is where you can showcase your music industry experience, collaborations, or resume highlights.</p>
+        </div>
+    )
+}
+
+export default Contact
