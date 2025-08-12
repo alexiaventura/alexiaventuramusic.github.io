05aa700caaa60cd15e6bd0f29a98ff3fa2451c56
Alexia Ventura <alexiaventura212@gmail.com>
2025-08-11 20:57:01 -0700

    Changing base

--- musicwebsite/vite.config.js
+++ musicwebsite/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/alexiaventuramusic.github.io",
+  base: "/",
   plugins: [react()],
 })
 
